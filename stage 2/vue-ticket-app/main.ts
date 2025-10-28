19ca19b3d1ce5cb9bd05db8b05d9030b1aab0c27
Lifeofstephanie <stephanieanyanwu605@gmail.com>
2025-10-28 13:12:11 +0100

    'vue'

--- stage 2/vue-ticket-app/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue"
-import App from "./app.vue"
-import router from "./router"
-import "./app/globals.css"
-
-const app = createApp(App)
-app.use(router)
-app.mount("#app")
--- stage 2/vue-ticket-app/vite.config.js
+++ stage 2/vue-ticket-app/vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import IconsResolver from "unplugin-icons/resolver";
+import Components from "unplugin-vue-components/vite";
+import Icons from "unplugin-icons/vite";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [IconsResolver()],
+    }),
+    Icons(),
+  ],
   build: {
     outDir: "dist",
   },
